3840b97f1bd8088cd75504c5c5daa8a332020a54
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-03-19 19:03:24 +0530

    Update First negative integer in every window of size k.cpp

--- First negative integer in every window of size k.cpp
+++ First negative integer in every window of size k.cpp
@@ -1,9 +1,9 @@
 //TC: O(N)
-//SC: O(1)
+//SC: O(K)
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
     long long int i=0, j=0;
-    queue<long long> q;
+    queue<long long> q; //stores list of negative numbers in the window
     vector<long long> ans;
     
     while(j<N){
@@ -14,12 +14,12 @@ vector<long long> printFirstNegativeInteger(long long int A[],
             j++;
             
         else if(j-i+1 == K){
-            if(!q.size())
+            if(!q.size())  //if No negative number present in the window, store 0
                 ans.push_back(0);
             else
-                ans.push_back(q.front());
+                ans.push_back(q.front());  //First negative number will be in the beginning of the queue
                 
-            if(q.front() == A[i])
+            if(q.front() == A[i])  //If the element to be excluded is in the front, it won't be the first hegative no. of the next window, so pop it
                 q.pop();
            i++;
            j++;
